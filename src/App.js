5f3b2f4426024b95e9a4bd22ec32764c1f093818
vanguyen333 <vanguyen333@yahoo.com>
2024-05-17 22:17:16 -0400

    #11 Conditional in jsx

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ function App() {
     { id: 1, name: 'abc 1' },
     { id: 2, name: 'abc 2' },
   ];
+  const loading = false;
+  if (loading) return <h1>Loading ...</h1>;
+  const showComments = true;
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments({comments.length})</h3>
+      {comments.map((item, index) => (
+        <div key={index}>
+          <li>{item.name}</li>
+        </div>
+      ))}
+    </div>
+  );
   return (
     <>
       <div className="container">
         <h1>{title}</h1>
         <p>{body}</p>
-        <div>
-          {comments.map((item, index) => (
-            <div key={index}>
-              <p>{item.id}</p>
-              <p>{item.name}</p>
-            </div>
-          ))}
-        </div>
+        {showComments && commentBlock}
       </div>
     </>
   );
