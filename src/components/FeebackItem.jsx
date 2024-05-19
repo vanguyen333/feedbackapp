cfa6f8bdc672a6db0da5a257e48b302b4d6e7676
vanguyen333 <vanguyen333@yahoo.com>
2024-05-18 22:26:19 -0400

    #14 b) useState & Hook with reset button

--- src/components/FeebackItem.jsx
+++ src/components/FeebackItem.jsx
@@ -10,12 +10,16 @@ const FeebackItem = () => {
       return prev + 1;
     });
   };
+  const reset = () => {
+    return setRating(7);
+  };
   return (
     // Card Container
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
       <button onClick={handleClick}>Click</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
