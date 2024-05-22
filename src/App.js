f6534fa2f59dd4e10635a75983ff4cde8ac38f80
vanguyen333 <vanguyen333@yahoo.com>
2024-05-21 22:48:57 -0400

    #26 Upgrading to React Router 6

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+
 function App() {
-  const [feedback, setFeebdack] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?'))
-      setFeebdack(feedback.filter((item) => item.id !== id));
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
   };
+
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
-    setFeebdack([newFeedback, ...feedback]);
+    setFeedback([newFeedback, ...feedback]);
   };
+
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
+
 export default App;
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,14 @@
+import Card from '../components/shared/Card';
+
+const AboutPage = () => {
+  return (
+    <Card>
+      <h1>About this project</h1>
+      <p>This is a react app to leave feedback for a product or service</p>
+      <p>Verion: 1.0.0 </p>
+      <a href="/">Back to Home Page</a>
+    </Card>
+  );
+};
+
+export default AboutPage;
